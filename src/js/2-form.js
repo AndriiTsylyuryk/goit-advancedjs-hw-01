ffb3550a7582d5f78894ba92527681ff9d2c329c
Andrii Tsylyuryk <andrii.tsylyuryk@pipedrive.com>
2025-10-20 09:25:18 +0300

    Refacror task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,49 @@
 const formData = {
-  email: '',
-  message: '',
+  email: "",
+  message: "",
 };
 
-const form = document.querySelector('.feedback-form');
-const textarea = form.querySelector('.message');
+const form = document.querySelector(".feedback-form");
+const textarea = form.querySelector(".message");
 
 populateInfo();
 
-form.addEventListener('input', handleInput);
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("input", handleInput);
+form.addEventListener("submit", handleSubmit);
 
 function handleInput(event) {
   const element = event.target.name;
   formData[element] = event.target.value;
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
 
 function populateInfo() {
-  const info = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const info = JSON.parse(localStorage.getItem("feedback-form-state"));
   if (info) {
     const { email, message } = form.elements;
-    email.value = info.email;
-    message.value = info.message;
+    email.value = info.email || "";
+    message.value = info.message || "";
+    formData.email = info.email || "";
+    formData.message = info.message || "";
   }
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const { email, message } = form.elements;
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
+  if (email.value === "" || message.value === "") {
+    return alert("Please fill in all the fields!");
   }
-  localStorage.clear('feedback-form-state')
+
+  formData.email = email.value;
+  formData.message = message.value;
+
+  console.log(formData);
+
+  formData.email = "";
+  formData.message = "";
+
+  localStorage.removeItem("feedback-form-state");
   form.reset();
 }
